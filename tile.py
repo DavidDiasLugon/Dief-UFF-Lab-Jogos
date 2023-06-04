32fb7da77e26440f4bbf2cf7ab82079b9f6c8e6a
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2023-06-04 19:41:18 -0300

    Update tile.py

--- tile.py
+++ tile.py
@@ -4,9 +4,7 @@ from config import *
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
-        super().__init__(groups)  # init so que da herança "pygame.sprite.Sprite"
-        self.image = pygame.image.load("../sprites/test/rock5.png").convert_alpha()
+        super().__init__(groups)  # init só que da herança "pygame.sprite.Sprite"
+        self.image = pygame.image.load("../Assets/Enviroment/rock5.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
-    
-
