7e74c14ba4834648888e9843e037b73648208edc
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2023-06-04 19:39:54 -0300

    Update level.py

--- level.py
+++ level.py
@@ -44,30 +44,30 @@ class Level:
                     Bau((x,Y),[self.visible_sprites,self.obstacles_sprites])
 
     def run(self):
-        # atualiza e desenha o jogo
+        # Atualiza e desenha o jogo
         self.visible_sprites.custom_draw(self.player)  # draw(self.display_surface)
         self.visible_sprites.update()
 
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
-        # general setup
+        # General setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
-        #criando o chao
-        self.floor_surf = pygame.image.load('tile_fixed.png').convert()
+        # Criando o chão
+        self.floor_surf = pygame.image.load('../Assets/Enviroment/tile_fixed.png').convert()
         self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
     def custom_draw(self, player):
-        # fazendo o recorte
+        # Fazendo o recorte
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        #desenhando o chao
+        # Desenhando o chão
         floor_offset_pos = self.floor_rect.topleft - self.offset
         self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
