2067750fd738dc79621ed67f8fe0a10559e8ab9e
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2023-06-04 20:38:28 -0300

    Update chave.py

--- chave.py
+++ chave.py
@@ -5,7 +5,7 @@ from config import *
 class Chave(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)  # init so que da herança "pygame.sprite.Sprite"
-        self.image = pygame.image.load("../Assets/Key/pixel-art-key/key.png").convert_alpha()
+        self.image = pygame.image.load("Assets/Itens/Key/key.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (30, 40))
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
