2f8c8849f00be9a06d8538d942d5dcd9f0a46eff
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2023-06-04 19:37:23 -0300

    Update cavaleiro.py

--- cavaleiro.py
+++ cavaleiro.py
@@ -5,8 +5,7 @@ from config import *
 class Cavaleiro(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)  # init so que da herança "pygame.sprite.Sprite"
-        self.image = pygame.image.load("knight_left.png").convert_alpha()
+        self.image = pygame.image.load("../Assets/Enemies/knight_left.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (64, 64))
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
-    
