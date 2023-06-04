4929831f4e8c1f00b2710eda50e9228f4f663376
David Dias <58055711+DavidDiasLugon@users.noreply.github.com>
2023-06-04 20:36:07 -0300

    Update bau.py

--- bau.py
+++ bau.py
@@ -5,7 +5,7 @@ from config import *
 class Bau(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)  # init so que da herança "pygame.sprite.Sprite"
-        self.image = pygame.image.load("../Assets/Chest/Chest1.png").convert_alpha()
+        self.image = pygame.image.load("Assets/Itens/Chest/Chest1.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (50, 40))
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
