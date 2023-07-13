de75ecd4007a10d53be15d307662ad280a3e7446
Luno58 <lucasavelino@id.uff.br>
2023-07-13 17:18:35 -0300

    funciona desgraça

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from level import Level
 from dief import *
 from PPlay import *
 from random import randint
+from menu import menu_inic
 
 
 class Game:
@@ -13,10 +14,11 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Dief")
         self.clock = pygame.time.Clock()
-
         self.level = Level()
+        self.menu = False
 
     def run(self):
+        return_to_menu = False
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -28,7 +30,19 @@ class Game:
             pygame.display.update()
             self.clock.tick(FPS)
 
+            if self.level.player.has_opened_chest():
+                return_to_menu = True
+                break
+            
+        if return_to_menu:
+            menu_result = menu_inic()
+            if not menu_result:
+                pygame.quit()
+                sys.exit()
+
 
 if __name__ == "__main__":
-    game = Game()
-    game.run()
+    menu_result = menu_inic()
+    if menu_result:
+        game = Game()
+        game.run()
